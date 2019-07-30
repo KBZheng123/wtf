57fb743b375722f8619c1c5998c7fc35fac2b01e
MSI GP63 <MSI GP63@10.40.48.197>
2019-07-30 14:19:30 +0700

    change to index

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
   <title>PHP Test</title>
  </head>
  <body>
+     <h1>
+         Hello cloud computing
+     </h1>
  <?php echo '<p>Hello World from Netbeans</p>'; 
     putenv("DATABASE_URL=fred");
     echo getenv("DATABASE_URL")
