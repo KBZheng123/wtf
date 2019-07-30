ca351e4584c30da771c59e49b9217d179afce1d6
MSI GP63 <MSI GP63@10.40.48.197>
2019-07-30 14:44:42 +0700

    change to index

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
      <h1>
          Hello cloud computing
      </h1>
-     <a href="https:///vnexpress.net/"> Vnepress</a>
+     <a href="https://vnexpress.net/"> Vnepress</a>
  <?php echo '<p>Hello World from Netbeans</p>'; 
     putenv("DATABASE_URL=fred");
     echo getenv("DATABASE_URL")
