a865497656b520a7d46940afd8e5ba320f8c55aa
MSI GP63 <MSI GP63@10.40.49.0>
2019-08-20 15:45:17 +0700

    change to index

--- deleteproductdata.php
+++ deleteproductdata.php
@@ -4,6 +4,12 @@
 
 <h1>INSERT DATA TO DATABASE</h1>
 
+<ul>
+ <form name="insertproductdata" action="insertproductdata.php" method="POST" >
+<li>Product ID to delete:</li><li><input type="text" name="productID" /></li>
+</form>
+</ul>
+
 <?php
 ini_set('display_errors', 1);
 echo "Insert database!";
@@ -16,19 +22,20 @@ if (empty(getenv("DATABASE_URL"))){
     echo '<p>The DB does not exist</p>';
     $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
 }  else {
-     
+     echo '<p>The DB exists</p>';
+     echo getenv("dbname");
    $db = parse_url(getenv("DATABASE_URL"));
    $pdo = new PDO("pgsql:" . sprintf(
-        "host=ec2-174-129-240-67.compute-1.amazonaws.com;port=5432;user=wrflrxtavasvqh;password=fbfef36049fbd28f1200e3a775a389e014838e86522765e67782f9cf7a3f516b;dbname=d3mmhribgmc6bf",
+        "host=ec2-54-227-251-33.compute-1.amazonaws.com;port=5432;user=vnkmjonwfpwbuo;password=cd185cb699a7b564675f0c0c86de18da272c5125cb6c0b13fd8e806f34861a89;dbname=dd80n3h55g0rvh",
         $db["host"],
         $db["port"],
         $db["user"],
         $db["pass"],
         ltrim($db["path"], "/")
    ));
-}  
+} 
 
-$sql = "DELETE FROM student WHERE stuid = 'SV02'";
+$sql = "DELETE FROM products WHERE productID = '$_POST[productID]'";
 $stmt = $pdo->prepare($sql);
 if($stmt->execute() == TRUE){
     echo "Record deleted successfully.";
