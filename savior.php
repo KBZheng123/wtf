74f01d30e96335ecce358d5cc67723d00fb5f399
MSI GP63 <MSI GP63@MSI>
2019-08-20 08:53:00 +0700

    change to index

--- savior.php
+++ savior.php
@@ -6,7 +6,7 @@
 
 <?php
 ini_set('display_errors', 1);
-echo "Hello Cloud computing class 0821";
+echo "Hello Cloud computing class 0818!";
 ?>
 
 <?php
@@ -20,7 +20,7 @@ if (empty(getenv("DATABASE_URL"))){
      echo getenv("dbname");
    $db = parse_url(getenv("DATABASE_URL"));
    $pdo = new PDO("pgsql:" . sprintf(
-        "host=ec2-54-227-251-33.compute-1.amazonaws.com;port=5432;user=vnkmjonwfpwbuo;password=cd185cb699a7b564675f0c0c86de18da272c5125cb6c0b13fd8e806f34861a89;dbname=dd80n3h55g0rvh",
+        "host=ec2-174-129-240-67.compute-1.amazonaws.com;port=5432;user=wrflrxtavasvqh;password=fbfef36049fbd28f1200e3a775a389e014838e86522765e67782f9cf7a3f516b;dbname=d3mmhribgmc6bf",
         $db["host"],
         $db["port"],
         $db["user"],
@@ -29,24 +29,23 @@ if (empty(getenv("DATABASE_URL"))){
    ));
 }  
 
-$sql = "SELECT * FROM public.products";
+$sql = "SELECT * FROM student ORDER BY stuid";
 $stmt = $pdo->prepare($sql);
 //Thiết lập kiểu dữ liệu trả về
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
 $stmt->execute();
 $resultSet = $stmt->fetchAll();
-echo '<p>Product information:</p>';
+echo '<p>Students information:</p>';
 
 ?>
 <div id="container">
 <table class="table table-bordered table-condensed">
     <thead>
       <tr>
-        <th>productID</th>
-        <th>productName</th>
-        <th>price</th>
-        <th>description</th>
-        <th>image</th>
+        <th>Student ID</th>
+        <th>Name</th>
+        <th>email</th>
+        <th>Class</th>
       </tr>
     </thead>
     <tbody>
@@ -57,11 +56,10 @@ echo '<p>Product information:</p>';
       ?>
    
       <tr>
-        <td scope="row"><?php echo $row['productID'] ?></td>
-        <td><?php echo $row['productName'] ?></td>
-        <td><?php echo $row['price'] ?></td>
-        <td><?php echo $row['description'] ?></td>
-        <td><?php echo $row['image'] ?></td>
+        <td scope="row"><?php echo $row['stuid'] ?></td>
+        <td><?php echo $row['fname'] ?></td>
+        <td><?php echo $row['email'] ?></td>
+        <td><?php echo $row['classname'] ?></td>
         
       </tr>
      
